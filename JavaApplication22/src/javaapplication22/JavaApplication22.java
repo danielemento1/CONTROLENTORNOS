fed44659e24324bbd56ca41af5ca9a1ee52a8d1c
danielemento <danielemento@192.168.1.137>
2020-02-07 07:11:15 +0100

    primer cambio

--- JavaApplication22/src/javaapplication22/JavaApplication22.java
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -16,6 +16,7 @@ public class JavaApplication22 {
      */
     public static void main(String[] args) {
         System.out.println("primer cambio");
+        System.out.println("cambioooo");
     }
     
 }
